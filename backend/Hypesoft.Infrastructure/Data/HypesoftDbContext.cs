6cc0fcd0e8ddaa8c9be9a1483e8a17bb8d4b5e07
Pedro Heringer <heringer.dev@icloud.com>
2026-01-15 22:29:51 -0300

    chore(mongo): configurando mongo

--- backend/Hypesoft.Infrastructure/Data/HypesoftDbContext.cs
+++ backend/Hypesoft.Infrastructure/Data/HypesoftDbContext.cs
@@ -1,6 +1,19 @@
+using Hypesoft.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+
 namespace Hypesoft.Infrastructure.Data;
 
-public class HypesoftDbContext
+public sealed class HypesoftDbContext(DbContextOptions<HypesoftDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Category> Categories { get; init; } = null!;
+    public DbSet<Product> Products { get; init; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>().ToCollection("categories");
+        modelBuilder.Entity<Product>().ToCollection("products");
+    }
 }
--- backend/Hypesoft.Infrastructure/Data/MongoOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace Hypesoft.Infrastructure.Data;
-
-public class MongoOptions
-{
-    
-}
