3ba21457ece322a0c49c5b33420f00adb7578c81
Pedro Heringer <heringer.dev@icloud.com>
2026-01-15 22:08:13 -0300

    feat(application): criei o validator do request de criar categoria

---
+++ Hypesoft.Application/Validators/Categories/CreateCategoryRequestValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using Hypesoft.Application.DTOs;
+
+namespace Hypesoft.Application.Validators.Categories;
+
+public sealed class CreateCategoryRequestValidator : AbstractValidator<CreateCategoryRequest>
+{
+    public CreateCategoryRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("O nome é obrigatório.")
+            .MaximumLength(80).WithMessage("O nome deve conter no minímo 80 caracteres.");
+    }
+}
